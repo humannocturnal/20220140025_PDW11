18d08cca0095afbc084e6b12bffe272fa578ea00
humannocturnal <syaputrar374@gmail.com>
2024-06-13 13:05:26 +0700

    praktikum

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://media.tenor.com/tCL3HGcaV4UAAAAM/raccoon-dance.gif" className="App-logo" alt="logo" />
+        <p>
+          Hai <code>Nama Saya</code> Ryan syaputra maulana arsad
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+ 
